cfb6734cf28f997cdefd3a014354afe2d9fa69df
NTH931 <noahth931@gmail.com>
2024-06-05 15:02:39 +1200

    Fixed

--- KeyboardButtons.js
+++ KeyboardButtons.js
@@ -1,15 +1,15 @@
 javascript:(function() {
 document.addEventListener('keydown', e => {
-    if (event.isComposing ||event.keyCode == 81) {
+    if (e.key == 'q') {
         document.querySelector('[data-functional-selector="answer-0"]').click();
     }
-    if (event.isComposing ||event.keyCode == 65) {
+    if (e.key == 'a') {
         document.querySelector('[data-functional-selector="answer-1"]').click();
     }
-    if (event.isComposing ||event.keyCode == 87) {
+    if (e.key == 'w') {
         document.querySelector('[data-functional-selector="answer-2"]').click();
     }
-    if (event.isComposing ||event.keyCode == 83) {
+    if (e.key == 's') {
         document.querySelector('[data-functional-selector="answer-3"]').click();
     }
 });
