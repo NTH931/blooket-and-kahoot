d9b9cc963cfdd3bca13811d6de20d9dcfeccf761
NTH931 <noahth931@gmail.com>
2024-06-05 14:58:40 +1200

    Fix

--- KeyboardButtons.js
+++ KeyboardButtons.js
@@ -1,15 +1,15 @@
 javascript:(function() {
 document.addEventListener('keydown', e => {
-    if (event.isComposing ||e.keyCode == 81) {
+    if (event.isComposing ||event.keyCode == 81) {
         document.querySelector('[data-functional-selector="answer-0"]').click();
     }
-    if (event.isComposing ||e.keyCode == 65) {
+    if (event.isComposing ||event.keyCode == 65) {
         document.querySelector('[data-functional-selector="answer-1"]').click();
     }
-    if (event.isComposing ||e.keyCode == 87) {
+    if (event.isComposing ||event.keyCode == 87) {
         document.querySelector('[data-functional-selector="answer-2"]').click();
     }
-    if (event.isComposing ||e.keyCode == 83) {
+    if (event.isComposing ||event.keyCode == 83) {
         document.querySelector('[data-functional-selector="answer-3"]').click();
     }
 });
